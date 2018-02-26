cbb65073d545ac248dd9842c9ff1c663e43168d5
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2018-02-26 10:59:36 -0800

    Checkout specific branch

--- ndnsim_setup.sh
+++ ndnsim_setup.sh
@@ -14,9 +14,9 @@ mkdir $ndnsim_root_dir
 cd $ndnsim_root_dir
 unset ndnsim_root_dir
 
-git clone https://github.com/named-data-ndnSIM/ns-3-dev.git ns-3
-git clone https://github.com/named-data-ndnSIM/pybindgen.git pybindgen
-git clone --recursive https://github.com/named-data-ndnSIM/ndnSIM.git ns-3/src/ndnSIM
+git clone -b ndnSIM-2.5 https://github.com/named-data-ndnSIM/ns-3-dev.git ns-3
+git clone -b 0.18.0 https://github.com/named-data-ndnSIM/pybindgen.git pybindgen
+git clone -b ndnSIM-2.5 --recursive https://github.com/named-data-ndnSIM/ndnSIM.git ns-3/src/ndnSIM
 
 cd ns-3
 ./waf configure -d optimized
