098341dbe16f6587fcee84dc51f6087bbd7ea5f1
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2018-03-05 13:39:58 -0800

    Update commands

--- ndnsim_setup.sh
+++ ndnsim_setup.sh
@@ -8,23 +8,24 @@ sudo apt-get install python-dev python-pygraphviz python-kiwi python-pygoocanvas
 #For the visualizer...
 sudo apt install gtk2-engines-pixbuf
 
+pwd=`pwd`
+
 echo "Installing ndnSIM"
-export ndnsim_root_dir=ndnSIM
+ndnsim_root_dir=$pwd/ndnSIM
 mkdir $ndnsim_root_dir && cd $ndnsim_root_dir
-unset ndnsim_root_dir
 
 git clone -b ndnSIM-2.5 https://github.com/named-data-ndnSIM/ns-3-dev.git ns-3
 git clone -b 0.18.0 https://github.com/named-data-ndnSIM/pybindgen.git pybindgen
 git clone -b ndnSIM-2.5 --recursive https://github.com/named-data-ndnSIM/ndnSIM.git ns-3/src/ndnSIM
 
-cd ns-3
+cd $ndnsim_root_dir/ns-3
 ./waf configure -d optimized
 ./waf
 sudo ./waf install
 
-cd ..
+cd $ndnsim_root_dir
 git clone --recursive https://github.com/akshayraman/scenario-ntorrent.git
-cd scenario-ntorrent
+cd $ndnsim_root_dir/scenario-ntorrent
 ./waf configure
 #./waf --run ntorrent-simple --vis
 
