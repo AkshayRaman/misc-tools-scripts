f610730c6c58fc7dea6854657e9042cd5e64ef7e
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2018-03-08 11:33:50 -0800

    Add miniconda script for mac

---
+++ miniconda_mac.sh
@@ -0,0 +1,13 @@
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+sh Miniconda3-latest-MacOSX-x86_64.sh
+
+. ~/.bash_profile
+
+conda create -n env python=3.6
+
+source activate env
+
+conda install tensorflow
+pip install keras
+pip install h5py
+
